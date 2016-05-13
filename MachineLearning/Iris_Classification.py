e5694b82cd548d27af677c3289439e66112b5ac2
Shruti Marwaha <shruti.marwaha@gmail.com>
2016-05-13 14:21:41 -0700

    add repository for scikit machine learning codes

---
+++ MachineLearning/Iris_Classification.py
@@ -0,0 +1,58 @@
+from __future__ import print_function, division
+from sklearn import datasets
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.svm import SVC #Support Vector Classification
+from sklearn.metrics import confusion_matrix
+
+# input data for scikit is n_samples, n_features array where samples are rows and features are columns.
+iris = datasets.load_iris()
+iris.keys()
+iris.values()
+
+# model features
+iris.feature_names
+iris_features = iris.data
+type(iris_features)
+iris_features.shape
+
+# response variables / classes you want to predict
+iris.target_names
+iris_classes = iris.target
+iris_classes.shape
+
+# Data Visualization
+x_index = 0
+y_index = 1
+
+# this formatter will label the colorbar with the correct target names
+formatter = plt.FuncFormatter(lambda i, *args: iris.target_names[int(i)])
+
+plt.scatter(iris_features[:, x_index], iris_features[:, y_index],
+            c=iris_classes, cmap=plt.cm.get_cmap('RdYlBu', 3))
+plt.colorbar(ticks=[0, 1, 2], format=formatter)
+plt.clim(-0.5, 2.5)
+plt.xlabel(iris.feature_names[x_index])
+plt.ylabel(iris.feature_names[y_index])
+
+# create the model
+#SVC?
+model = SVC()
+print (model)
+
+# fit the model
+model.fit(iris_features,iris_classes)
+
+# What kind of iris has 3cm x 5cm sepal and 4cm x 2cm petal?
+# call the "predict" method:
+result = model.predict([[3,5,4,2],])
+print(iris.target_names[result])
+
+print (model.predict(iris_features[::10]))
+print ((iris_classes[::10]))
+
+predicted_classes = model.predict(iris_features)
+# numpy.all : Test whether all array elements along a given axis evaluate to True.
+print(np.all(iris_classes == predicted_classes))
+
+print (confusion_matrix (iris_classes,predicted_classes) )
