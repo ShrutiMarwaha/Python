30338141c4078c2ddcb866ac434933a9465fb55a
Shruti Marwaha <shruti.marwaha@gmail.com>
2016-06-22 21:39:31 -0700

    more efficient way

--- kmer.py
+++ kmer.py
@@ -1,29 +1,31 @@
 # Given a file of reads, find reads which have complete match of sequences for k bases.
+
 def kmer(seq_list,id_list,k):
-    match = []
-    matched_bases = {}
+    # dictionary containing different "k" bases from each sequence
+    kseq = {}
+    # dictionary containing k bases which are common across sequences
+    matched_seq = {}
 
-    # compare sequences to all the following sequences in the list
-    for i in range(len(seq_list)-1):
-        # loop through each index in ith sequence to select k elements. there is no need to look last k-1 indices
+    for i in range(len(seq_list)):
         for j in range(len(seq_list[i])-(k-1)):
-            # search for the above k elements in each of following sequences in the list
-            for m in range(i+1,len(seq_list)):
-                if seq_list[i][j:j+k] in seq_list[m]:
-                    # print seq_list[i][j:j+k]
-                    # print seq_list[m]
-                    # print "\n"
-                    match.append([seq_list[i][j:j+k], id_list[i], id_list[m]])
+            if seq_list[i][j:j+k] not in kseq:
+                # if the k-bases are not present in the dictionary, add the k-bases as key and the sequence id as value.
+                # since the values should be unique, store the values as set and not as lists
+                kseq[ seq_list[i][j:j+k] ] = {id_list[i]}
+            else:
+                kseq[seq_list[i][j:j+k]].add(id_list[i])
 
-                    if seq_list[i][j:j+k] not in matched_bases:
-                        matched_bases[seq_list[i][j:j+k]] = [ id_list[i], id_list[m] ]
-                    else:
-                        matched_bases[seq_list[i][j:j+k]].append([id_list[i], id_list[m]])
+    for key, value in kseq.iteritems():
+        if len(value) >1:
+            matched_seq[key] = value
 
-    #print match
-    return matched_bases
+    return(matched_seq)
 
-sequence_list = ["ATGCTAGC", "TTGCATA", "CTGCTAT"]
+
+sequence_list = ["ATGCTAGC", "TTGCATA", "CTGCTGAT"]
 identifier_list = ["seq1","seq2","seq3"]
 
 print kmer(sequence_list,identifier_list,3)
+
+
+
